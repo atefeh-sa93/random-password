06a895c0a01b49b8ebf6cdf3a0977a1dcf3406a3
“[atefeh <[atefeh@buzzjective.com]>
2021-10-03 22:33:51 +0330

    install vuetify again and sass, fix the generatePassword component and styles

--- src/components/GeneratePassword.vue
+++ src/components/GeneratePassword.vue
@@ -1,198 +1,117 @@
 <template>
-    <div>
-        <section class="section section section-shaped my-0 overflow-hidden">
-            <div class="shape shape-style-1 bg-gradient-warning shape-skew">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+  <div class="main-content">
+    <v-container>
+      <v-row class="align-center">
+        <v-col cols="12" md="6">
+          <div class="d-flex px-3">
+            <div class="intro-icon">
+              <v-icon color="deep-purple darken-4">
+                mdi-lock-open-outline
+              </v-icon>
             </div>
-            <div class="container py-0">
-                <div class="row align-items-center">
+            <div class="pl-3">
+              <h4 class="display-1 font-weight-black white--text">
+                Generate Password
+              </h4>
+              <p class="white--text text-center font-weight-bold">
+                Simple random password generator
+              </p>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-img src="@/assets/images/pass-img-bg.svg" class="intro-img" />
+        </v-col>
+      </v-row>
 
-                    <!--Man Image!-->
-                    <div class="col-md-6 order-lg-2 ml-lg-auto">
-                        <div class="position-relative pl-md-5">
-                            <img src="@/assets/pass-img-bg.svg"
-                                 class="img-center img-fluid"
-                                 alt="man generate password">
-                        </div>
-                    </div>
-                    <!--Man Image!-->
-
-                    <!--Introduction!-->
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="d-flex px-3">
-
-                            <!-- Generate Password Icon!-->
-                            <div>
-                                <div class="icon icon-shape bg-gradient-white icon-lg shadow rounded-circle text-icon">
-                                    <i class="material-icons">lock_open</i>
-                                </div>
-                            </div>
-                            <!--Generate password Title introduction!-->
-                            <div class="pl-4">
-                                <h4 class="display-3 text-white">Generate Password</h4>
-                                <p class="text-white intro">Simple random password generator</p>
-                            </div>
-
-                        </div>
-                    </div>
-                    <!--Introduction!-->
-
+      <v-row class="card-box-handle">
+        <v-col cols="12" md="6">
+          <div class="generator-result">
+            <h4>Password</h4>
+            <v-row>
+              <v-col cols="12" md="12">
+                <div>
+                  <v-text-field
+                    name="input-10-2"
+                    label="Error"
+                    outlined
+                  ></v-text-field>
+                  <div class="buttons">
+                    <v-btn color="success">Generate Password</v-btn>
+                    <v-btn color="success">Copy Password</v-btn>
+                  </div>
                 </div>
-            </div>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
 
-            <!--generate password card!-->
-            <div class="container">
-
-                <div class="row card-box-handle">
-
-                    <!--Generate password Input!-->
-                    <div class="col-md-6 col-sm-6">
-                        <div class="generator">
-                            <div class="generator-result">
-                                <h4>Password</h4>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="input-group input-icon-bg">
-                                            <input id="generator-password"
-                                                   type="text"
-                                                   :value="password"
-                                                   disabled
-                                            >
-                                            <div class="buttons">
-                                                <button class="btn pass-btn"
-                                                        @click="generateNewPassword"
-                                                >
-                                                    Generate Password
-                                                </button>
-                                                <button class="btn pass-btn"
-                                                        @click="copyPassword"
-                                                >
-                                                    Copy Password
-                                                </button>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Options for generate password!-->
-                    <div class="col-md-6 col-sm-6">
-
-                        <div class="options">
-                            <h4 class="display-3">Options</h4>
-                            <div class="row o-m-15">
-                                <span>Password Length</span>
-                            </div>
-                            <div class="row o-m-15 length">
-                                <input type="range"
-                                       min="8"
-                                       max="50"
-                                       class="options-range"
-                                       v-model="defaultLength"
-
-                                >
-                                <input type="number"
-                                       min="8"
-                                       max="50"
-                                       class="options-number"
-                                       v-model="defaultLength"
-                                >
-                            </div>
-                            <div class="row o-m-15">
-                                <h6><label for="">Password content</label></h6>
-                            </div>
-                            <div class="row o-m-15 switch"
-                                 v-for="type in passwordContents"
-                                 :key="type.title"
-                            >
-                                <span>{{type.title}}</span>
-                                <label class="switch-items">
-                                    <input type="checkbox"
-                                           :checked="type.checked"
-                                           v-on:click="type.checked = !type.checked"
-                                    >
-                                    <span class="slider round"></span>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+        <v-col cols="12" md="6">
+          <div class="options">
+            <h4 class="display-3">Options</h4>
+            <span>Password Length</span>
+            <v-row>
+              <v-text-field
+                type="range"
+                min="8"
+                max="50"
+                class="options-range"
+                v-model="defaultLength"
+              ></v-text-field>
+              <v-text-field
+                type="number"
+                min="8"
+                max="50"
+                class="options-number"
+                v-model="defaultLength"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <h6><label for="">Password content</label></h6>
+              <v-row v-for="type in passwordContents" :key="type.title">
+                <span>{{ type.title }}</span>
+                <input
+                  type="checkbox"
+                  :checked="type.checked"
+                  v-on:click="type.checked = !type.checked"
+                />
+              </v-row>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: 'GeneratePassword',
-        /**
-         * @inheritDoc
-         * @returns {{defaultLength: number, passwordContents: [{checked: boolean, title: string, value: string}, {checked: boolean, title: string, value: string}, {checked: boolean, title: string, value: string}, {checked: boolean, title: string, value: string}]}}
-         */
-        data() {
-            return {
-                defaultLength   : 8,
-                passwordContents: [
-                    {
-                        title  : 'UpperCase',
-                        value  : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-                        checked: false,
-                    },
-                    {
-                        title  : 'LowerCase',
-                        value  : 'abcdefghijklmnopqrstuwxyz',
-                        checked: true,
-                    },
-                    {
-                        title  : 'Numbers',
-                        value  : '0123456789',
-                        checked: true,
-                    },
-                    {
-                        title  : 'Symbols',
-                        value  : '`!@#$%^&*)(-_+=}{][/|?><',
-                        checked: false,
-                    },
-                ],
-                password        : '',
-            };
+export default {
+  data() {
+    return {
+      defaultLength: 8,
+      passwordContents: [
+        {
+          title: "UpperCase",
+          value: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+          checked: false,
         },
-        methods: {
-            /**
-             * Generate New Random Password
-             */
-            generateNewPassword() {
-                let resultPass = '';
-                let charsVal = '';
-                for (let i = 0; i < this.passwordContents.length; i++) {
-                    if (this.passwordContents[i].checked) {
-                        charsVal += this.passwordContents[i].value;
-                    }
-                }
-                for (let j = 0; j < this.defaultLength; j++) {
-                    resultPass += charsVal.charAt(Math.floor(Math.random() * charsVal.length));
-                }
-                this.password = resultPass;
-            },
-            /**
-             * Copy Password
-             */
-            copyPassword() {
-                let copyPassText = this.password;
-                navigator.clipboard.writeText(copyPassText).then(() => {
-                    console.log('Copied!');
-                }, function () {
-                    console.log('Error happen');
-                });
-            },
+        {
+          title: "LowerCase",
+          value: "abcdefghijklmnopqrstuwxyz",
+          checked: true,
         },
+        {
+          title: "Numbers",
+          value: "0123456789",
+          checked: true,
+        },
+        {
+          title: "Symbols",
+          value: "`!@#$%^&*)(-_+=}{][/|?><",
+          checked: false,
+        },
+      ],
+      password: "",
     };
+  },
+};
 </script>
