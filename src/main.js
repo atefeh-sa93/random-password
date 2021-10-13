1a01d51dca9fb47e0f6f360098255e2f34532993
“[atefeh <[atefeh@buzzjective.com]>
2021-10-13 10:17:45 +0330

    import v-gtag package and use it

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 
+Vue.use(VueGtag, {
+  config: { id: "G-0HLF8N6CR5" },
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
